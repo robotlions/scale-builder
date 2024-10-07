804eebd67cfff3bb24844c513d66edd1cfee70e0
Chad M <chadmusick@gmail.com>
2024-10-07 02:47:02 -0400

    replaced multiple buttons with array map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import "./App.css";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 const intToKey = {
-  1 : "C",
+  1: "C",
   2: "Db",
   3: "D",
   4: "Eb",
@@ -15,59 +15,130 @@ const intToKey = {
   9: "Ab",
   10: "A",
   11: "Bb",
-  12: "B"
-}
+  12: "B",
+};
 
-
+const keyArray = [
+  { idNo: 1, keyName: "C" },
+  { idNo: 2, keyName: "Db" },
+  { idNo: 3, keyName: "D" },
+  { idNo: 4, keyName: "Eb" },
+  { idNo: 5, keyName: "E" },
+  { idNo: 6, keyName: "F" },
+  { idNo: 7, keyName: "Gb" },
+  { idNo: 8, keyName: "G" },
+  { idNo: 9, keyName: "Ab" },
+  { idNo: 10, keyName: "A" },
+  { idNo: 11, keyName: "Bb" },
+  { idNo: 12, keyName: "B" },
+];
 
 function App() {
+  const [currentKey, setCurrentKey] = useState(1);
+  const [currentScale, setCurrentScale] = useState("Major");
+  const [active, setActive] = useState(1);
 
-  const [currentKey, setCurrentKey] = useState(1)
-  const [currentScale, setCurrentScale] = useState("Major")
-  const [buttonClass, setButtonClass] = useState("btn btn-secondary")
-
-  function convertKey(interval){
-   let x = currentKey+interval
-   if (x > 12){
-    x=x-12
-   }
-   return x
+  function convertKey(interval) {
+    let x = currentKey + interval;
+    if (x > 12) {
+      x = x - 12;
+    }
+    return x;
   }
+
+  const KeyButton = ({ id, keyName, isActive, value }) => {
+    return (
+      <button
+        id={id}
+        value={value}
+        type="button"
+        className={
+          isActive ? "btn btn-secondary buttonActive" : "btn btn-secondary"
+        }
+        onClick={(e) => {
+          setCurrentKey(Number(e.target.value));
+          setActive(Number(e.target.value));
+        }}
+      >
+        {keyName}
+      </button>
+    );
+  };
 
   return (
     <div className="container App">
       <div class="row">
         <div class="col">
-      <p>Current Key = {intToKey[currentKey]}</p>
-      <p>Current Scale = {currentScale}</p>
-      </div></div>
-      <div class="row"><div class="col">
+          {/* <p>Current Key = {intToKey[currentKey]}</p> */}
+          <p>Current Scale = {currentScale}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          {keyArray.map((item) => (
+            <KeyButton
+              isActive={active === item.idNo}
+              keyName={item.keyName}
+              value={item.idNo}
+            />
+          ))}
+        </div>
+        </div>
+        <br />
+        <br />
+        <div class="row justify-content-center">
+          <div class="col-1">
+        <p>1</p>
+        </div>
+       
+          <div class="col-1">
+        <p>2</p>
+        </div>
       
-      <button type="button" class={buttonClass} value={1} onClick={(e)=>setCurrentKey(Number(e.target.value))}>C</button>
-      <button type="button" class={buttonClass} value={2} onClick={(e)=>setCurrentKey(Number(e.target.value))}>Db/C#</button>
-      <button type="button" class={buttonClass} value={3} onClick={(e)=>setCurrentKey(Number(e.target.value))}>D</button>
-      <button type="button" class={buttonClass} value={4} onClick={(e)=>setCurrentKey(Number(e.target.value))}>Eb/D#</button>
-      <button type="button" class={buttonClass} value={5} onClick={(e)=>setCurrentKey(Number(e.target.value))}>E</button>
-      <button type="button" class={buttonClass} value={6} onClick={(e)=>setCurrentKey(Number(e.target.value))}>F</button>
-      <button type="button" class={buttonClass} value={7} onClick={(e)=>setCurrentKey(Number(e.target.value))}>Gb/F#</button>
-      <button type="button" class={buttonClass} value={8} onClick={(e)=>setCurrentKey(Number(e.target.value))}>G</button>
-      <button type="button" class={buttonClass} value={9} onClick={(e)=>setCurrentKey(Number(e.target.value))}>Ab/G#</button>
-      <button type="button" class={buttonClass} value={10} onClick={(e)=>setCurrentKey(Number(e.target.value))}>A</button>
-      <button type="button" class={buttonClass} value={11} onClick={(e)=>setCurrentKey(Number(e.target.value))}>Bb/A#</button>
-      <button type="button" class={buttonClass} value={12} onClick={(e)=>setCurrentKey(Number(e.target.value))}>B</button>
-
-</div>
-<br />
-     <br/>
-     <p>1 = {intToKey[currentKey]}</p>
-     <p>2 = {intToKey[convertKey(2)]} minor</p>
-     <p>3 = {intToKey[convertKey(4)]} minor</p>
-     <p>4 = {intToKey[convertKey(5)]}</p>
-     <p>5 = {intToKey[convertKey(7)]}</p>
-     <p>6 = {intToKey[convertKey(9)]} minor</p>
-     <p>7 = {intToKey[convertKey(11)]} diminished</p>
-    </div>
-    </div>
+          <div class="col-1">
+        <p>3</p>
+        </div>
+       
+          <div class="col-1">
+        <p>4</p>
+        </div>
+        <div class="col-1">
+        <p>5</p>
+        </div>
+        <div class="col-1">
+        <p>6</p>
+        </div>
+        <div class="col-1">
+        <p>7</p>
+        </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-1">
+        <p>{intToKey[currentKey]}</p>
+        </div>
+       
+          <div class="col-1">
+        <p>{intToKey[convertKey(2)]}m</p>
+        </div>
+      
+          <div class="col-1">
+        <p>{intToKey[convertKey(4)]}m</p>
+        </div>
+       
+          <div class="col-1">
+        <p>{intToKey[convertKey(5)]}</p>
+        </div>
+        <div class="col-1">
+        <p>{intToKey[convertKey(7)]}</p>
+        </div>
+        <div class="col-1">
+        <p>{intToKey[convertKey(9)]}m</p>
+        </div>
+        <div class="col-1">
+        <p>{intToKey[convertKey(11)]}dim</p>
+        </div>
+        </div>
+      </div>
   );
 }
 
